169828bc2d79f180488b58543e146b4cb243f946
jorge <jorge@jorge-desktop>
2016-09-24 16:33:23 -0300

    .

--- src/main/java/app/view/DateFormatter.java
+++ src/main/java/app/view/DateFormatter.java
@@ -4,17 +4,28 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.Formatter;
 
 public class DateFormatter implements Formatter<Date> {
 
+	@Autowired
+    private Logger logger = Logger.getLogger(DateFormatter.class.getName());
+	
 	public Date parse(final String text, final Locale locale) throws ParseException {
+		
+		logger.info("executando parse do DateFormatter");
+		
 		final SimpleDateFormat dateFormat = dateFormat();
 		return dateFormat.parse(text);
 	}
 
 	public String print(final Date object, final Locale locale) {
+		
+		logger.info("executando print do DateFormatter");
+		
 		final SimpleDateFormat dateFormat = dateFormat();
 		return dateFormat.format(object);
 	}
